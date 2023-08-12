751052f185c19f0498768b6e07c50a5ac5093e9b
honsmart <adegboyeopeyemi580@gmail.com>
2023-08-12 01:42:40 +0100

    Update, Retrive and delete url

--- todos/urls.py
+++ todos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from todos.views import TodosAPIView
+from todos.views import TodosAPIView, TodoDetailAPIView
 
 
 urlpatterns = [
-     path('', TodosAPIView.as_view(), name="todo")
+     path('', TodosAPIView.as_view(), name="todos"),
+     path('<int:id>', TodoDetailAPIView.as_view(), name="todo")
+
 ]
