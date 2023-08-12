373e188b417dc16e5017c42edfdda79801413922
honsmart <adegboyeopeyemi580@gmail.com>
2023-08-12 01:27:43 +0100

    Todo Api view

--- todos/views.py
+++ todos/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView
 from todos.serializers import TodoSerializer
 from rest_framework.permissions import IsAuthenticated
+from todos.models import Todo
 
-# Create your views here.
-class CreateTodoAPIView(CreateAPIView):
-    serializer_class = TodoSerializer
-    permission_classes = (IsAuthenticated,)
+class TodosAPIView(ListCreateAPIView):
+     serializer_class = TodoSerializer
+     permission_classes = (IsAuthenticated,)
 
-    def perform_create(self, serializer):
+     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
-
-
-    
+     
+     def get_queryset(self):
+        return Todo.objects.filter(owner=self.request.user)
