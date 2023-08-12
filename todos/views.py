c4a1fc1124f6d1ed396c544a66cbeda8de796e0b
honsmart <adegboyeopeyemi580@gmail.com>
2023-08-12 03:06:25 +0100

    filters

--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from todos.pagination import CustomPageNumberPagination
+from rest_framework import filters
 from todos.serializers import TodoSerializer
 from rest_framework.permissions import IsAuthenticated
 from todos.models import Todo
+from django_filters.rest_framework import DjangoFilterBackend
 
 class TodosAPIView(ListCreateAPIView):
      serializer_class = TodoSerializer
+     pagination_class = CustomPageNumberPagination
      permission_classes = (IsAuthenticated,)
+     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+     filterset_fields = ['id', 'title','desc', 'is_complete']
+     search_fields = ['id', 'title','desc', 'is_complete']
+     ordering_fields = ['id', 'title','desc', 'is_complete']
+
+
 
      def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
