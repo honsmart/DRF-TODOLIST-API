25b199822658b234d0a010fe6dcfd219e5643616
honsmart <adegboyeopeyemi580@gmail.com>
2023-08-15 03:53:29 +0100

    added table fields

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from authentication.models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ('username','first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined','email_verified')
+    search_fields = ('username','first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined','email_verified')
+    list_per_page =25
+
+
+
+admin.site.register(User, UserAdmin)
