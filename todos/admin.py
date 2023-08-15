f6689d1fc53a2e2bdec4520ba3e24a35a5c377fb
honsmart <adegboyeopeyemi580@gmail.com>
2023-08-15 03:54:33 +0100

    Add fileds to todo table admin

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from todos.models import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+
+    list_display = ('title','desc', 'is_complete', 'owner')
+    search_fields = ('title','desc', 'is_complete', 'owner')
+    list_per_page =25
 
 # Register your models here.
+admin.site.register(Todo, TodoAdmin)
